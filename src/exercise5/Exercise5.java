751d50a2cf9df84020da21ee769f28a3dd193e60
fatima.roman <fatima.roman@106-26.info2>
2025-09-19 12:58:14 +0200

    ex5

---
+++ src/exercise5/Exercise5.java
@@ -0,0 +1,12 @@
+package exercise5;
+
+public class Exercise5 {
+
+	public static void main(String[] args) {
+		System.out.println("Fátima Román");
+		System.out.println("Avda. Cruz del campo");
+		System.out.println("624264192");
+
+	}
+
+}
